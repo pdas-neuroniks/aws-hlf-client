f173a4e8ce08b3be146d25908b16c5c06959aacf
Papan Das <pdas@neuroniks.com>
2025-11-04 23:06:10 +0530

    update

--- scripts/enroll-admin.js
+++ scripts/enroll-admin.js
@@ -14,24 +14,24 @@ const caName = utilities.getCaName(caEndpoint);
 
 // Enroll the admin only on creation
 exports.handler = async (event) => {
-  if (event.RequestType === 'Create') {
-    try {
-      // Get the TLS cert from S3
-      const caTlsCert = await utilities.getS3Object(tlsCertBucket, tlsCertKey);
-      console.log('caTlsCert', caTlsCert)
-      // Get the admin credentials from Secrets Manager
-      const adminPwd = await utilities.getSecret(adminPasswordArn);
-      console.log('adminPwd',adminPwd)
-      // Create a new client for interacting with the CA
-      const ca = new FabricCAClient(caUrl, { trustedRoots: caTlsCert, verify: false }, caName);
-      console.log('ca',ca)
-      // Enroll the admin user, and import the new identity into Secrets Manager
-      const enrollment = await ca.enroll({ enrollmentID: 'admin123', enrollmentSecret: adminPwd });
-      console.log('enrollment',enrollment)
-      await utilities.putSecret(privateKeyArn, enrollment.key.toBytes());
-      await utilities.putSecret(signedCertArn, enrollment.certificate);
-    } catch (error) {
-      console.error(`Failed to enroll admin user: ${error}`);
-    }
+  
+  try {
+    // Get the TLS cert from S3
+    const caTlsCert = await utilities.getS3Object(tlsCertBucket, tlsCertKey);
+    console.log('caTlsCert', caTlsCert)
+    // Get the admin credentials from Secrets Manager
+    const adminPwd = await utilities.getSecret(adminPasswordArn);
+    console.log('adminPwd',adminPwd)
+    // Create a new client for interacting with the CA
+    const ca = new FabricCAClient(caUrl, { trustedRoots: caTlsCert, verify: false }, caName);
+    console.log('ca',ca)
+    // Enroll the admin user, and import the new identity into Secrets Manager
+    const enrollment = await ca.enroll({ enrollmentID: 'admin123', enrollmentSecret: adminPwd });
+    console.log('enrollment',enrollment)
+    await utilities.putSecret(privateKeyArn, enrollment.key.toBytes());
+    await utilities.putSecret(signedCertArn, enrollment.certificate);
+  } catch (error) {
+    console.error(`Failed to enroll admin user: ${error}`);
   }
+
 };
