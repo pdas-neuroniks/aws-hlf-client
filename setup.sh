f23ae3eadf7f6aaef5dd6eb48cd6e5bd2c58df46
Ubuntu <ubuntu@ip-10-0-2-53.ec2.internal>
2025-11-05 19:01:05 +0000

    setup file

---
+++ setup.sh
@@ -0,0 +1,10 @@
+sudo apt-get update
+sudo apt-get upgrade -y
+sudo apt install unzip -y
+sudo apt-get install git -y
+sudo apt-get install curl -y
+sudo apt-get install docker.io -y
+sudo apt install docker-compose -y
+sudo apt install tree -y
+docker --version
+docker-compose --version
