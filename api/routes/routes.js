3c9e4ce2ef316a328a41ddaef2f4dfe82d75d02b
Papan Das <pdas@neuroniks.com>
2025-11-07 05:07:48 +0530

    update routes

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -18,7 +18,10 @@ router.get('/ping', function(rew, res){
     }
 })
 
-const ctrlNetwork = require('../blockchain/network/info');
-router.get('/network/chain-info', ctrlNetwork.getChainInfo)
+// const ctrlEnroll = require('../blockchain/network/enroll');
+// router.post('/network/enroll', ctrlEnroll.enroll)
+
+// const ctrlNetwork = require('../blockchain/network/info');
+// router.get('/network/chain-info', ctrlNetwork.getChainInfo)
 
 module.exports = router;
