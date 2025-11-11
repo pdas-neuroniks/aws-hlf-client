6d4fd561a899eb74717abad0f332e28183202b11
Papan Das <pdas@neuroniks.com>
2025-11-11 22:59:01 +0530

    update

--- scripts/enroll-admin.sh
+++ scripts/enroll-admin.sh
@@ -1,5 +1,5 @@
 fabric-ca-client enroll \
-    -u 'https://admin:A6minPw8A6minPw@ca.m-3pv7agmbbnan3eqyf3x3llddqu.n-jyv5ovyrazfkxl4bpwzhok3t7y.managedblockchain.us-east-1.amazonaws.com:30002' 
+    -u 'https://admin:A6minPw8A6minPw@ca.m-ga2tf7fkbzbxtcydzhxebi3v6a.n-3ibx7ieydjdnborkil7y46iu7u.managedblockchain.us-east-1.amazonaws.com:30002' 
     --tls.certfiles /home/ubuntu/resource/managedblockchain-tls-chain.pem \
     -M /home/ubuntu/resource/admin-msp
 
@@ -7,11 +7,11 @@ cp -r /home/ubuntu/resource/admin-msp/signcerts /home/ubuntu/resource/admin-msp/
 
 
 
-curl https://ca.m-3pv7agmbbnan3eqyf3x3llddqu.n-jyv5ovyrazfkxl4bpwzhok3t7y.managedblockchain.us-east-1.amazonaws.com:30002/cainfo -k
+curl https://ca.m-ga2tf7fkbzbxtcydzhxebi3v6a.n-3ibx7ieydjdnborkil7y46iu7u.managedblockchain.us-east-1.amazonaws.com:30002/cainfo -k
 
 
 fabric-ca-client enroll \
-  -u "https://admin:$(aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:108625623765:secret:BlockcedeDevHLFNetworkAdmin-bxnQ9cO3hDiI-ARLHYa --query SecretString --output text)@ca.m-3pv7agmbbnan3eqyf3x3llddqu.n-jyv5ovyrazfkxl4bpwzhok3t7y.managedblockchain.us-east-1.amazonaws.com:30002" \
+  -u "https://admin:$(aws secretsmanager get-secret-value --secret-id arn:aws:secretsmanager:us-east-1:108625623765:secret:BlockcedeDevHLFNetworkAdmin-Bfc6bEGFPAEs-KOP0pg --query SecretString --output text)@ca.m-ga2tf7fkbzbxtcydzhxebi3v6a.n-3ibx7ieydjdnborkil7y46iu7u.managedblockchain.us-east-1.amazonaws.com:30002" \
   --tls.certfiles /home/ubuntu/resource/managedblockchain-tls-chain.pem \
   -M /home/ubuntu/resource/admin-msp
 
