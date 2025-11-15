53b9a7fc734cafcc002f8f452414e5b3fa07e58d
Papan Das <pdas@neuroniks.com>
2025-11-15 09:34:31 +0530

    update

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config('./.env');
-console.log(process.env)
+// console.log(process.env)
 
 var indexRouter = require('./api/routes/routes');
 
